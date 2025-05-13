3d75afc86c3ac84595f1e8b15866635a1b99b7fd
Yash <ycode37@gmail.com>
2025-05-13 23:10:04 +0530

    Commiting First Nodejs

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  fs.readdir(`./files`, (err, files) => {
+    console.log(files);
+    res.render('index', { files: files });
+  });
+});
+
+app.post('/create', (req, res) => {
+  // console.log(req.body);
+  fs.writeFile(
+    `./files/${req.body.title.split(' ').join('')}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect('/');
+    }
+  );
+});
+
+app.get('/file/:filename', (req, res) => {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    'utf-8',
+    function (err, filedata) {
+      res.render('show', {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    }
+  );
+});
+
+app.listen(3000);
